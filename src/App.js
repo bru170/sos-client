4bf3d7d42553640eb80aba6c339745429a6d7067
bru170 <antoniabruno92@gmail.com>
2022-03-25 23:38:14 +0100

    added footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./styles/index.scss"
 import {Routing} from "./pages/Routes/Routing"
 import Navbar from "./components/Nav/Navbar"
+import Footer from "./components/Footer/Footer"
 
 function App() {
   return (
     <>
       <Navbar />
       <Routing />
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+import styles from "./Footer.module.scss"
+const Footer = () => (
+  <footer className={styles.footer}>
+    <div className={styles.grid}>
+      <div className={styles.grid__items}></div>
+    </div>
+  </footer>
+)
+
+export default Footer
